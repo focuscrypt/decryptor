51a2930871a7bd7185c6915a9da4a01be5acc9b4
Koray Üstündağ <mskorayustundag@gmail.com>
2021-03-21 19:53:53 +0300

    update v1.3.2.1

---
+++ Focus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace decryptor
+{
+    public static class Focus
+    {
+        private const int Keysize = 256;
+
+        private const int DerivationIterations = 1000;
+
+        public static string Decrypt(string cipherText, string passPhrase)
+        {
+            try
+            {
+                byte[] cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+                byte[] saltStringBytes = cipherTextBytesWithSaltAndIv.Take(32).ToArray();
+                byte[] ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(32).Take(16).ToArray();
+                byte[] cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip(32 + 16).Take(cipherTextBytesWithSaltAndIv.Length - (32 + 16)).ToArray();
+
+                Rfc2898DeriveBytes password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations);
+
+                byte[] keyBytes = password.GetBytes(Keysize / 8);
+                using (RijndaelManaged symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.BlockSize = 128;
+                    symmetricKey.Mode = CipherMode.CBC;
+                    symmetricKey.Padding = PaddingMode.PKCS7;
+                    using (ICryptoTransform decryptorU = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream(cipherTextBytes))
+                        {
+                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptorU, CryptoStreamMode.Read))
+                            {
+                                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                memoryStream.Close();
+                                cryptoStream.Close();
+                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return "Decrypt Error";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace decryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Focus::Decryptor";
+            ConsoleColor white = ConsoleColor.White;
+            ConsoleColor red = ConsoleColor.Red;
+            ConsoleColor green = ConsoleColor.Green;
+            Console.WriteLine("FocusCrypt [Version 1.3.2.1]");
+            Console.WriteLine("https://www.github.com/focuscrypt/decryptor");
+            Console.ForegroundColor = green;
+            Console.WriteLine("Women's Rights Matters.");
+            Console.WriteLine();
+            Console.ForegroundColor = white;
+        start:
+            Console.Write("Encrypted Text:");
+            string etext = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(etext))
+            {
+                Console.ForegroundColor = red;
+                Console.WriteLine("Please enter any text.");
+                Console.ForegroundColor = white;
+                Console.WriteLine();
+                goto start;
+            }
+            Console.WriteLine();
+        second:
+            Console.Write("Password:");
+            string pass = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+                if (key == ConsoleKey.Backspace && pass.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    pass = pass[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    pass += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+            Console.WriteLine();
+            if (string.IsNullOrWhiteSpace(pass))
+            {
+                Console.ForegroundColor = red;
+                Console.WriteLine("Please enter a password!");
+                Console.ForegroundColor = white;
+                Console.WriteLine();
+                goto second;
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = green;
+            Console.WriteLine("Ok.");
+            Console.ForegroundColor = white;
+            Console.WriteLine("The decryption process has started. Please wait...");
+            Console.WriteLine("The decryption process is complete. Result:");
+            Console.WriteLine();
+            Console.ForegroundColor = green;
+            Console.WriteLine("-----");
+            Console.ForegroundColor = white;
+            Console.WriteLine(Focus.Decrypt(etext, pass));
+            Console.ForegroundColor = green;
+            Console.WriteLine("-----");
+            Console.WriteLine();
+            Console.ForegroundColor = white;
+            Console.Write("Do you want to do another decryption?(y/n)");
+            string c = Console.ReadLine();
+            if (c == "y")
+            {
+                Console.WriteLine();
+                goto start;
+            }
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
